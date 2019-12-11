1e11d613b77584bfd2b8fe6d1fb08c6bb6ce2666
NH4L <nh4l@qq.com>
2019-12-11 22:07:06 +0800

    image serviceImpl

---
+++ src/main/java/cn/aysst/bghcs/serviceImpl/ImageServiceImpl.java
@@ -0,0 +1,25 @@
+package cn.aysst.bghcs.serviceImpl;
+
+import cn.aysst.bghcs.dao.ImageDao;
+import cn.aysst.bghcs.entity.Image;
+import cn.aysst.bghcs.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author lcy
+ * @version 2019-12-11
+ */
+@Component
+public class ImageServiceImpl implements ImageService {
+
+    @Autowired
+    ImageDao imageDao;
+
+    @Override
+    public String insertImage(List<Image> list, String userOpenId) {
+        return imageDao.insertImage(list, userOpenId);
+    }
+}
