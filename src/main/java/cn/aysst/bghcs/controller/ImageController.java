151d3565d8a69d577dd145c896f8b005c37ec953
NH4L <nh4l@qq.com>
2019-12-11 22:03:46 +0800

    图片controller

--- src/main/java/cn/aysst/bghcs/controller/ImageController.java
+++ src/main/java/cn/aysst/bghcs/controller/ImageController.java
@@ -1,18 +1,19 @@
 package cn.aysst.bghcs.controller;
 
 import cn.aysst.bghcs.entity.Image;
+import cn.aysst.bghcs.service.ImageService;
 import cn.aysst.bghcs.util.*;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import static cn.aysst.bghcs.util.FileGenertor.genericPath;
@@ -24,45 +25,62 @@ import static cn.aysst.bghcs.util.FileGenertor.genericPath;
 @RequestMapping(value = "/image", method = {RequestMethod.GET, RequestMethod.POST})
 public class ImageController {
 
+    @Autowired
+    ImageService imageService;
+
     @RequestMapping("/upload")
-    public Result upload(@RequestParam MultipartFile file, String userOpenId, HttpServletRequest request) {
+    @ResponseBody
+    public Result upload(@RequestParam MultipartFile[] files, String userOpenId, HttpServletRequest request) {
         String path = request.getContextPath();
         String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path;
 
-        if (file != null) {
-            if (file.getSize() > 1024 * 1024 * 10) {
-                System.out.println("文件上传失败");
-                return ResultUtils.fail(ResultCode.FAIL, "文件过大，请上传10M以内的图片");
-            }
-            if (file.isEmpty()) {
-                return ResultUtils.fail(ResultCode.FAIL, "未选择图片");
-            } else {
-                try {
-                    // 生成文件名
-                    String originalFilename = FileGenertor.generateFilename(file);
-                    //得出两级目录地址: /3/4
-                    String realPath = genericPath(originalFilename, Config.LOCAL_PATH + userOpenId + "/");
+        List<Image> list = new ArrayList<Image>();
+        for (MultipartFile file: files) {
+            try {
+                if (file != null) {
+                    if (file.getSize() > 1024 * 1024 * 10) {
+                        System.out.println("文件上传失败");
+                        return ResultUtils.fail(ResultCode.FAIL, "文件过大，请上传10M以内的图片");
+                    }
+                    if (file.isEmpty()) {
+                        System.out.println("文件上传失败");
+                        return ResultUtils.fail(ResultCode.FAIL, "未选择图片");
+                    }
 
-                    //磁盘物理地址
-                    String diskFileDir = Config.LOCAL_PATH + userOpenId + "/" + realPath + originalFilename;
-                    //网络访问地址
-                    String networkFileDir = basePath + "/static/images/" + userOpenId + "/"+ realPath + originalFilename;
-
-                    File dest = new File(diskFileDir);
-                    file.transferTo(dest);
-                    Image image = new Image();
-                    image.setImageName(originalFilename);
-                    image.setImageAddr(networkFileDir);
-                    return ResultUtils.success(image);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return ResultUtils.fail(ResultCode.FAIL, "文件上传失败");
                 }
-            }
+                // 生成文件名
+                String originalFilename = FileGenertor.generateFilename(file);
+                //得出两级目录地址: /3/4
+                String realPath = genericPath(originalFilename, Config.LOCAL_PATH + userOpenId + "/");
 
+                //磁盘物理地址
+                String diskFileDir = Config.LOCAL_PATH + userOpenId + "/" + realPath + originalFilename;
+                //网络访问地址
+                String networkFileDir = basePath + "/static/images/" + userOpenId + "/"+ realPath + originalFilename;
+
+                File dest = new File(diskFileDir);
+                file.transferTo(dest);
+                System.out.println("上传文件成功");
+                Image image = new Image();
+                image.setImageName(originalFilename);
+                image.setImageUrl(networkFileDir);
+                list.add(image);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ResultUtils.fail(ResultCode.FAIL, "文件上传失败");
+            }
         }
-        return ResultUtils.fail(ResultCode.FAIL);
+        if (list.isEmpty())
+            return ResultUtils.fail(ResultCode.FAIL, "未选择图片");
+        else {
+            String isSuccess = imageService.insertImage(list, userOpenId);
+            if (isSuccess.equals("success"))
+                return ResultUtils.success(list);
+            else
+                return ResultUtils.fail(ResultCode.FAIL, "数据库访问繁忙");
+        }
+
     }
 
 }
