5d940cde7b86886f99e5eb94de98c694c1346aa6
NH4L <nh4l@qq.com>
2019-12-11 22:06:40 +0800

    image service layer

---
+++ src/main/java/cn/aysst/bghcs/service/ImageService.java
@@ -0,0 +1,17 @@
+package cn.aysst.bghcs.service;
+
+import cn.aysst.bghcs.entity.Image;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author lcy
+ * @version 2019-12-11
+ */
+@Service
+public interface ImageService {
+
+    String insertImage(List<Image> list, String userOpenId);
+
+}
