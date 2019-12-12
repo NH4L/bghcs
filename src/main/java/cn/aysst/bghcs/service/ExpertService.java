0ee9a11d5d440908d8c3186fe034d9be3f1d4c21
NH4L <nh4l@qq.com>
2019-12-12 21:48:47 +0800

    新增专家service层

---
+++ src/main/java/cn/aysst/bghcs/service/ExpertService.java
@@ -0,0 +1,16 @@
+package cn.aysst.bghcs.service;
+
+import cn.aysst.bghcs.entity.Expert;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lcy
+ * @version 2019-12-12
+ * 专家服务层
+ */
+@Service
+public interface ExpertService {
+
+    String insertExpert(Expert expert);
+
+}
