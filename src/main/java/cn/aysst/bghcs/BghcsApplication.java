ef7250e6daad7dadd63a21163d7b52271fb78500
NH4L <nh4l@qq.com>
2019-12-13 22:19:19 +0800

    启动类

--- src/main/java/cn/aysst/bghcs/BghcsApplication.java
+++ src/main/java/cn/aysst/bghcs/BghcsApplication.java
@@ -2,12 +2,19 @@ package cn.aysst.bghcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BghcsApplication {
+public class BghcsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BghcsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
 }
