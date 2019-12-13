fbc0e1ebeede21f8773f8cb2a40d0ea9f5bf2420
NH4L <nh4l@qq.com>
2019-12-13 22:16:38 +0800

    专家Dao层

--- src/main/java/cn/aysst/bghcs/dao/ExpertDao.java
+++ src/main/java/cn/aysst/bghcs/dao/ExpertDao.java
@@ -4,6 +4,7 @@ import cn.aysst.bghcs.entity.Expert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
@@ -21,24 +22,51 @@ public class ExpertDao {
     private JdbcTemplate jdbcTemplate;
 
     public String insertExpert(Expert expert) {
-        String insertSql = "INSERT INTO expert(expertName, expertEducation, expertJobTitle)\n" +
-                "VALUES (?, ?, ?)";
-        int count = jdbcTemplate.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement preparedStatement = connection.prepareStatement(insertSql, new String[]{"expertId"});
-                preparedStatement.setString(1, expert.getExpertName());
-                preparedStatement.setString(2, expert.getExpertEducation());
-                preparedStatement.setString(3, expert.getExpertJobTitle());
-                return preparedStatement;
-            }
-        });
+        String querySql = "SELECT *\n" +
+                "FROM expert\n" +
+                "WHERE expertEmail=?";
 
-        if (count > 0) {
-            return "success";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(querySql, expert.getExpertEmail());
+        if (!result.next()) {
+            String insertSql = "INSERT INTO expert(expertEmail, expertPassword, expertName, expertEducation, expertJobTitle)\n" +
+                    "VALUES (?, ?, ?, ?, ?)";
+            int count = jdbcTemplate.update(new PreparedStatementCreator() {
+                @Override
+                public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                    PreparedStatement preparedStatement = connection.prepareStatement(insertSql, new String[]{"expertId"});
+                    preparedStatement.setString(1, expert.getExpertEmail());
+                    preparedStatement.setString(2, expert.getExpertPassword());
+                    preparedStatement.setString(3, expert.getExpertName());
+                    preparedStatement.setString(4, expert.getExpertEducation());
+                    preparedStatement.setString(5, expert.getExpertJobTitle());
+                    return preparedStatement;
+                }
+            });
+
+            if (count > 0) {
+                return "success";
+            } else {
+                return "fail";
+            }
         } else {
-            return "fail";
+            return "emailExists";
         }
     }
 
+    public Expert login(String expertEmail, String expertPassword) {
+        Expert expert = new Expert();
+        String querySql = "SELECT *\n" +
+                "FROM expert\n" +
+                "WHERE expertEmail=? AND expertPassword=?";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(querySql, expertEmail, expertPassword);
+        if (result.next()) {
+            expert.setExpertId(result.getInt("expertId"));
+            expert.setExpertEmail(result.getString("expertEmail"));
+            expert.setExpertName(result.getString("expertName"));
+            expert.setExpertEducation(result.getString("expertEducation"));
+            expert.setExpertJobTitle(result.getString("expertJobTitle"));
+            return expert;
+        } else
+            return null;
+    }
 }
