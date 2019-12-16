5d89acccd500e84bcf1e2f7484391b61a88230cd
NH4L <nh4l@qq.com>
2019-12-16 22:45:04 +0800

    专家serviceImpl

--- src/main/java/cn/aysst/bghcs/serviceImpl/ExpertServiceImpl.java
+++ src/main/java/cn/aysst/bghcs/serviceImpl/ExpertServiceImpl.java
@@ -2,9 +2,14 @@ package cn.aysst.bghcs.serviceImpl;
 
 import cn.aysst.bghcs.dao.ExpertDao;
 import cn.aysst.bghcs.entity.Expert;
+import cn.aysst.bghcs.entity.Image;
 import cn.aysst.bghcs.service.ExpertService;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author lcy
@@ -24,5 +29,20 @@ public class ExpertServiceImpl implements ExpertService {
     @Override
     public Expert login(String expertEmail, String expertPassword) {
         return expertDao.login(expertEmail, expertPassword);
+    }
+
+    @Override
+    public String check(String expertEmail, String cropName, String diseaseName, String imageUrl) {
+        return expertDao.checkImage(expertEmail, cropName, diseaseName, imageUrl);
+    }
+
+    @Override
+    public Map getCheckedImage(String expertEmail, String imageUrl) {
+        return expertDao.getCheckedImage(expertEmail, imageUrl);
+    }
+
+    @Override
+    public List<Image> getAllImage(int page, int pageNum) {
+        return expertDao.getAllImage(page, pageNum);
     }
 }
