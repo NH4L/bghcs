1c8bdeffd53fbddd1d89e741287c358f9e740b2a
NH4L <nh4l@qq.com>
2019-12-16 22:40:43 +0800

    添加分页

--- src/main/java/cn/aysst/bghcs/controller/ExpertController.java
+++ src/main/java/cn/aysst/bghcs/controller/ExpertController.java
@@ -2,12 +2,18 @@ package cn.aysst.bghcs.controller;
 
 
 import cn.aysst.bghcs.entity.Expert;
+import cn.aysst.bghcs.entity.Image;
 import cn.aysst.bghcs.service.ExpertService;
 import cn.aysst.bghcs.util.Result;
 import cn.aysst.bghcs.util.ResultCode;
 import cn.aysst.bghcs.util.ResultUtils;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author lcy
@@ -21,6 +27,11 @@ public class ExpertController {
     @Autowired
     ExpertService expertService;
 
+    /**
+     * 专家注册
+     * @param expert
+     * @return
+     */
     @RequestMapping("/register")
     @ResponseBody
     public Result insertExpert(@RequestBody Expert expert) {
@@ -33,6 +44,12 @@ public class ExpertController {
             return ResultUtils.fail(ResultCode.FAIL, "服务器繁忙");
     }
 
+    /**
+     * 专家登录
+     * @param expertEmail
+     * @param expertPassword
+     * @return
+     */
     @RequestMapping("/login")
     @ResponseBody
     public Result login(@RequestParam String expertEmail, String expertPassword) {
@@ -41,6 +58,53 @@ public class ExpertController {
             return ResultUtils.success(expert);
         else
             return ResultUtils.fail(ResultCode.FAIL, "用户名或密码错误");
+    }
+
+    /**
+     * 专家检测图片为哪类病虫害
+     * @param expertEmail
+     * @param cropName
+     * @param diseaseName
+     * @param imageUrl
+     * @return success或fail
+     */
+    @RequestMapping("/check")
+    @ResponseBody
+    public Result check(@RequestParam String expertEmail, String cropName, String diseaseName, String imageUrl) {
+        String isSuccess = expertService.check(expertEmail, cropName, diseaseName, imageUrl);
+        if (isSuccess.equals("success"))
+            return ResultUtils.success();
+        else
+            return ResultUtils.fail(ResultCode.FAIL, "fail");
+    }
+
+    /**
+     * 获取已检测图片信息
+     * @param expertEmail 专家邮箱
+     * @param imageUrl 图片地址
+     * @return 图片信息
+     */
+    @RequestMapping("/get")
+    @ResponseBody
+    public Result getUserImage(@RequestParam String expertEmail, String imageUrl) {
+        Map result = expertService.getCheckedImage(expertEmail, imageUrl);
+        if (result != null)
+            return ResultUtils.success(result);
+        else
+            return ResultUtils.fail(ResultCode.FAIL, "null");
+    }
+
+    /**
+     * 实现分页
+     * @param page 当前页数
+     * @param pageNum 一页多少条数据
+     * @return 图片列表
+     */
+    @RequestMapping("/list")
+    @ResponseBody
+    public Result getAllImage(@RequestParam int page, int pageNum) {
+        List<Image> imageList = expertService.getAllImage(page, pageNum);
+        return ResultUtils.success(imageList);
     }
 
 }
