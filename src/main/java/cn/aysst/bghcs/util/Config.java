4c6a95cd3ed58bdcf49ab56b27e47b8d6e2f72a2
NH4L <nh4l@qq.com>
2019-12-22 09:32:10 +0800

    配置工具类

--- src/main/java/cn/aysst/bghcs/util/Config.java
+++ src/main/java/cn/aysst/bghcs/util/Config.java
@@ -9,6 +9,7 @@ public class Config {
     public static final String LOCAL_PATH = "e://HHU/ThirdYear/images/";
 //    public static final String LOCAL_PATH = "/usr/projects/bghcs/";
 
-    public static final String BASE_PATH = "http://localhost:8080/";
+    public static final String BASE_PATH = "http://localhost:8080/bghcs";
+//    public static final String BASE_PATH = "http://www.aysst.cn/bghcs";
 
 }
