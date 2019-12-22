ea31a4ded58b983be45ab7022678d947e74cad0c
NH4L <nh4l@qq.com>
2019-12-22 09:31:07 +0800

    用户service层

--- src/main/java/cn/aysst/bghcs/service/UserService.java
+++ src/main/java/cn/aysst/bghcs/service/UserService.java
@@ -3,6 +3,9 @@ package cn.aysst.bghcs.service;
 import cn.aysst.bghcs.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author lcy
  * @version 2019-12-9
@@ -13,4 +16,6 @@ public interface UserService {
     String insertUser(User user);
 
     String checkOpenId(String userOpenId);
+
+    List<Map> getCheckInfo(String userOpenId, String imageUrl);
 }
